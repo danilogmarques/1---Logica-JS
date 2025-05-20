10fe5c90ebe938c66bb8b00722f88705fd6b920a
danilogmarques <danilogustavomarques@gmail.com>
2025-05-20 20:23:22 -0300

    exercicio 9 finalizado

---
+++ exercicio9.js
@@ -0,0 +1,11 @@
+// 9 - Ler um valor e escrever se o mesmo é positivo e negativo.
+
+const prompt = require ('prompt-sync')();
+
+let valor = prompt ( 'Digite um valor ' );
+
+if( valor >= 0 ){
+    console.log(' O valor digitado é positivo ')
+}else {
+    console.log(' O valor digitado é negativo ')
+}
