8f5344f604aa54b9cfc7e59e2476e7f7a02c82d2
danilogmarques <danilogustavomarques@gmail.com>
2025-05-20 18:20:34 -0300

    terminado o exercicio 8

---
+++ exercicio8.js
@@ -0,0 +1,17 @@
+// 8 - Ler um valor e escrever " O valor é maior que 10 ", caso o valor informado seja maior que 10,
+// caso contrário informar "valor menor que 10"
+
+const prompt = require ('prompt-sync')()
+
+let valor = prompt("Digite um número ")
+
+if( valor >= 10 ){
+    console.log(` O valor digitado é maior que 10`)
+}else{
+    console.log(` O valor digitado é menor que 10`)
+}
+
+
+
+
+
