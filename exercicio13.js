3d22d51c68e3b77de2f631fb4cbe076959836db7
danilogmarques <danilogustavomarques@gmail.com>
2025-05-21 08:44:24 -0300

    exercicio 13 finalizado

--- exercicio13.js
+++ exercicio13.js
@@ -1,3 +1,23 @@
 // 13 - Faça um programa para ler a quantidade máxima, minima e atual de um estoque.
 // calcule a quantidade média M = (qtd maxi + qtd mini )/2 se a quantidade atual for 
-// maior ou igual a quantidade maior escrever "Não efetuar a compra" se estiver abaixo escrever "Efetuar a compra".
+// maior ou igual a quantidade máxima  escrever "Não efetuar a compra" se estiver abaixo escrever "Efetuar a compra".
+
+const prompt = require('prompt-sync')()
+
+let quantidadeMaxima  = prompt('digite a quantidade máxima do estoque ')
+quantidadeMaxima = Number(quantidadeMaxima)
+
+let quantidadeMinima = prompt('digite a quantidade minima do estoque ')
+quantidadeMinima = Number(quantidadeMinima)
+
+let quantidadeAtual = prompt('digite a quantido atual do estoque ')
+quantidadeAtual = Number(quantidadeAtual)
+
+media = ( quantidadeMaxima + quantidadeMinima ) / 2 
+media = Number(media)
+
+if( quantidadeAtual >= quantidadeMaxima  ){
+    console.log(`O estoque é de ${media} e está bom, não efetuar a comprar`)
+}else {
+    console.log(`O estoque é de ${media} e está baixo, efetuar a compra`)
+}
