6fac88e0d349c996cca1dc61079ccf9f1dc9f752
danilogmarques <danilogustavomarques@gmail.com>
2025-05-23 16:50:44 -0300

    exercicio 25 finalizado

---
+++ exercicio25.js
@@ -0,0 +1,9 @@
+// 25 - Escreva uma algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+// Utilizando ENQUANTO e um CONTADOR.
+
+let contador = 1 
+
+while ( contador <= 10) {
+    console.log(contador)
+    contador ++
+}
