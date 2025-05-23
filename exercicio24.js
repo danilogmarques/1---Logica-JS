da97b9a39c3994b45eb222df673a2e0680c7c336
danilogmarques <danilogustavomarques@gmail.com>
2025-05-23 15:58:31 -0300

    exercicio 24 finalizado

---
+++ exercicio24.js
@@ -0,0 +1,11 @@
+// 24 - Escreva um algoritmo para imprimir os número de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+// Utilizando Repita/Faça e um Contador 
+
+let contador = 1 
+
+do {
+    console.log(contador)
+    
+    contador ++
+} while ( contador <= 10) 
+
