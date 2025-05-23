cbe26dc89c8f65b347773eff2cdbb4c57e0ed9da
danilogmarques <danilogustavomarques@gmail.com>
2025-05-23 15:47:35 -0300

    exercicio 23 finalizado

---
+++ exercicio23.js
@@ -0,0 +1,11 @@
+// 23 - Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10)
+
+let numero = 8
+let resultado 
+
+for (contador = 1; contador <= 10; contador++){
+   resultado =  contador * numero 
+
+   console.log(`${numero} x ${contador} = ${resultado}`)
+    
+}
