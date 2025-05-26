4b77157ab25bc4d7a3a96894e75ddd402aec1a7f
danilogmarques <danilogustavomarques@gmail.com>
2025-05-26 09:40:56 -0300

    exercicio 33 finalizado

---
+++ exercicio33.js
@@ -0,0 +1,8 @@
+//33 - Escreva um algoritmo que imprima a tabuada ( de 1 a 10 ) para os números de 1 0 10.
+
+for (contador = 1; contador <= 10; contador ++) {
+    for (numero = 0; numero <= 10; numero ++){
+        resultado = contador*numero 
+        console.log (`${contador} x ${numero} = ${resultado}`)
+    }
+}
